afde5fc3d10b9951672f395466b24fccc7c771bb
Andrey Slyusarchuk <anray.nr@gmail.com>
2023-01-25 23:35:38 +0300

    initial

---
+++ TestingApp/TestingApp/TestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAppApp.swift
+//  TestingApp
+//
+//  Created by Andrey on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
